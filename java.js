d81f50d9d3b4d640033ac403581820955f7273c0
Mikael David <63308965+MikaelDDavidd@users.noreply.github.com>
2023-05-18 17:50:48 -0300

    sla

--- java.js
+++ java.js
@@ -11,3 +11,17 @@ menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navmenu.classList.toggle('open');
 }
+
+// Selecione o ícone de usuário e a mini janela
+const userIcon = document.querySelector('.bx-user');
+const userInfo = document.querySelector('.user-info');
+
+// Evento para exibir a mini janela ao passar o mouse sobre o ícone de usuário
+userIcon.addEventListener('mouseenter', function() {
+  userInfo.style.display = 'block';
+});
+
+// Evento para ocultar a mini janela ao retirar o mouse do ícone de usuário
+userIcon.addEventListener('mouseleave', function() {
+  userInfo.style.display = 'none';
+});
